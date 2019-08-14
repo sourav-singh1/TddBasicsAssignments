9abc26303ec90398b5db20d2c4cdd602e4189c82
Sourav Singh <53294757+sourav-singh1@users.noreply.github.com>
2019-08-14 16:08:43 +0530

    PasswordVerifier, PasswordVerifierTest, MyExceptionCustom
    Assignments

---
+++ MyException.java
@@ -0,0 +1,11 @@
+package com.yash.TddAssignments;
+
+public class MyException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	MyException(String message) {
+        super(message);
+    }
+
+}
